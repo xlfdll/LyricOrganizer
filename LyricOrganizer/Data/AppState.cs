5c0bc62e5250595b27b97dc5e7bb1f69ddc3e9d8
xlfdll <xlfdll@gmail.com>
2018-09-23 22:27:04 -0700

    Version 1.0: Updated namespaces according to the latest changes of submodules

--- LyricOrganizer/Data/AppState.cs
+++ LyricOrganizer/Data/AppState.cs
@@ -1,83 +1,83 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 
-using Xlfdll.Core;
+using Xlfdll;
 
 namespace LyricOrganizer
 {
-	public class AppState : ObservableObject
-	{
-		public AppState()
-		{
-			this.Status = "Ready";
-			this.TypeIndex = 0;
-			this.Page = 1;
+    public class AppState : ObservableObject
+    {
+        public AppState()
+        {
+            this.Status = "Ready";
+            this.TypeIndex = 0;
+            this.Page = 1;
 
-			this.Results = new ObservableCollection<LyricItem>();
-			this.Providers = new ObservableCollection<ILyricProvider>();
+            this.Results = new ObservableCollection<LyricItem>();
+            this.Providers = new ObservableCollection<ILyricProvider>();
 
-			this.InitializeProviders();
+            this.InitializeProviders();
 
-			AppState.Current = this;
-		}
+            AppState.Current = this;
+        }
 
-		#region UI States
+        #region UI States
 
-		private String _status;
-		private Boolean _isBusy;
-		private LyricItem _currentItem;
-		private Int32 _page;
+        private String _status;
+        private Boolean _isBusy;
+        private LyricItem _currentItem;
+        private Int32 _page;
 
-		public String Status
-		{
-			get { return _status; }
-			set { this.SetField<String>(ref _status, value); }
-		}
-		public Boolean IsBusy
-		{
-			get { return _isBusy; }
-			set { this.SetField<Boolean>(ref _isBusy, value); }
-		}
-		public LyricItem CurrentItem
-		{
-			get { return _currentItem; }
-			set { this.SetField<LyricItem>(ref _currentItem, value); }
-		}
-		public Int32 Page
-		{
-			get { return _page; }
-			set { this.SetField<Int32>(ref _page, value); }
-		}
+        public String Status
+        {
+            get { return _status; }
+            set { this.SetField<String>(ref _status, value); }
+        }
+        public Boolean IsBusy
+        {
+            get { return _isBusy; }
+            set { this.SetField<Boolean>(ref _isBusy, value); }
+        }
+        public LyricItem CurrentItem
+        {
+            get { return _currentItem; }
+            set { this.SetField<LyricItem>(ref _currentItem, value); }
+        }
+        public Int32 Page
+        {
+            get { return _page; }
+            set { this.SetField<Int32>(ref _page, value); }
+        }
 
-		#endregion
+        #endregion
 
-		#region Data Input
+        #region Data Input
 
-		private String _keyword;
-		private Int32 _typeIndex;
+        private String _keyword;
+        private Int32 _typeIndex;
 
-		public String Keyword
-		{
-			get { return _keyword; }
-			set { this.SetField<String>(ref _keyword, value); }
-		}
+        public String Keyword
+        {
+            get { return _keyword; }
+            set { this.SetField<String>(ref _keyword, value); }
+        }
 
-		public Int32 TypeIndex
-		{
-			get { return _typeIndex; }
-			set { this.SetField<Int32>(ref _typeIndex, value); }
-		}
+        public Int32 TypeIndex
+        {
+            get { return _typeIndex; }
+            set { this.SetField<Int32>(ref _typeIndex, value); }
+        }
 
-		#endregion
+        #endregion
 
-		public ObservableCollection<LyricItem> Results { get; }
-		public ObservableCollection<ILyricProvider> Providers { get; }
+        public ObservableCollection<LyricItem> Results { get; }
+        public ObservableCollection<ILyricProvider> Providers { get; }
 
-		private void InitializeProviders()
-		{
-			this.Providers.Add(new JLyricProvider());
-		}
+        private void InitializeProviders()
+        {
+            this.Providers.Add(new JLyricProvider());
+        }
 
-		public static AppState Current { get; private set; }
-	}
+        public static AppState Current { get; private set; }
+    }
 }
--- LyricOrganizer/Providers/JLyricProvider.cs
+++ LyricOrganizer/Providers/JLyricProvider.cs
@@ -4,89 +4,89 @@ using System.Collections.ObjectModel;
 using System.Text;
 using System.Text.RegularExpressions;
 
-using Xlfdll.Core.Network;
+using Xlfdll.Network;
 
 namespace LyricOrganizer
 {
-	public class JLyricProvider : ILyricProvider
-	{
-		public String Name => "J-Lyric.net";
+    public class JLyricProvider : ILyricProvider
+    {
+        public String Name => "J-Lyric.net";
 
-		public ReadOnlyCollection<LyricItem> Search(String keyword, LyricSearchType type)
-		{
-			return this.Search(keyword, type, null);
-		}
+        public ReadOnlyCollection<LyricItem> Search(String keyword, LyricSearchType type)
+        {
+            return this.Search(keyword, type, null);
+        }
 
-		public ReadOnlyCollection<LyricItem> Search(String keyword, LyricSearchType type, Int32? page)
-		{
-			List<LyricItem> results = new List<LyricItem>();
+        public ReadOnlyCollection<LyricItem> Search(String keyword, LyricSearchType type, Int32? page)
+        {
+            List<LyricItem> results = new List<LyricItem>();
 
-			Dictionary<String, String> query = new Dictionary<String, String>();
+            Dictionary<String, String> query = new Dictionary<String, String>();
 
-			switch (type)
-			{
-				case LyricSearchType.BySongName:
-					{
-						query.Add("kt", keyword);
-						break;
-					}
-				case LyricSearchType.ByArtist:
-					{
-						query.Add("ka", keyword);
-						break;
-					}
-				case LyricSearchType.ByLyric:
-					{
-						query.Add("kl", keyword);
-						break;
-					}
-				default:
-					break;
-			}
+            switch (type)
+            {
+                case LyricSearchType.BySongName:
+                    {
+                        query.Add("kt", keyword);
+                        break;
+                    }
+                case LyricSearchType.ByArtist:
+                    {
+                        query.Add("ka", keyword);
+                        break;
+                    }
+                case LyricSearchType.ByLyric:
+                    {
+                        query.Add("kl", keyword);
+                        break;
+                    }
+                default:
+                    break;
+            }
 
-			query.Add("ct", "2");
+            query.Add("ct", "2");
 
-			if (page != null)
-			{
-				query.Add("p", page.ToString());
-			}
+            if (page != null)
+            {
+                query.Add("p", page.ToString());
+            }
 
-			String result = WebOperations.GetContentAsString(JLyricProvider.QueryURI.ToString(), Encoding.UTF8, query);
+            String result = WebOperations.GetContentAsString(JLyricProvider.QueryURI.ToString(), Encoding.UTF8, query);
 
-			foreach (Match match in JLyricProvider.SearchParseRegex.Matches(result))
-			{
-				String songName = match.Groups["SongTitle"].Value;
-				String songArtist = match.Groups["SongArtist"].Value;
-				Uri uri = new Uri(JLyricProvider.QueryURI.GetLeftPart(UriPartial.Authority) + match.Groups["RelativePath"].Value);
+            foreach (Match match in JLyricProvider.SearchParseRegex.Matches(result))
+            {
+                String songName = match.Groups["SongTitle"].Value;
+                String songArtist = match.Groups["SongArtist"].Value;
+                Uri uri = new Uri(JLyricProvider.QueryURI.GetLeftPart(UriPartial.Authority) + match.Groups["RelativePath"].Value);
 
-				results.Add(new LyricItem(songName, songArtist, this, uri));
-			}
+                results.Add(new LyricItem(songName, songArtist, this, uri));
+            }
 
-			return results.AsReadOnly();
-		}
+            return results.AsReadOnly();
+        }
 
-		public LyricContent Retrieve(LyricItem item)
-		{
-			if (item.Provider.Name != this.Name)
-			{
-				throw new NotSupportedException("The lyric item data was not from the current provider.");
-			}
+        public LyricContent Retrieve(LyricItem item)
+        {
+            if (item.Provider.Name != this.Name)
+            {
+                throw new NotSupportedException("The lyric item data was not from the current provider.");
+            }
 
-			String html = WebOperations.GetContentAsString(item.URI.ToString(), Encoding.UTF8);
+            String html = WebOperations.GetContentAsString(item.URI.ToString(), Encoding.UTF8);
 
-			Match match = JLyricProvider.LyricParseRegex.Match(html);
+            Match match = JLyricProvider.LyricParseRegex.Match(html);
 
-			String title = item.SongTitle;
-			String artist = item.SongArtist;
-			String writer = match.Groups["Writer"].Value;
-			String composer = match.Groups["Composer"].Value;
-			String[] lyric = match.Groups["Lyric"].Value.Split(new String[] { "<br>" }, StringSplitOptions.None);
+            String title = item.SongTitle;
+            String artist = item.SongArtist;
+            String writer = match.Groups["Writer"].Value;
+            String composer = match.Groups["Composer"].Value;
+            String[] lyric = match.Groups["Lyric"].Value.Split(new String[] { "<br>" }, StringSplitOptions.None);
 
-			return new LyricContent(title, artist, writer, composer, lyric, item);
-		}
+            return new LyricContent(title, artist, writer, composer, lyric, item);
+        }
 
-		private static readonly Uri QueryURI = new Uri("http://j-lyric.net/index.php");
-		private static readonly Regex SearchParseRegex = new Regex(@"<div class='title'>\s*<a href='(?<RelativePath>.+)'>(?<SongTitle>.+)</a></div>\s*<div class='status'>\s*.+<a href='.+'>(?<SongArtist>.+)</a>", RegexOptions.Compiled);
-		private static readonly Regex LyricParseRegex = new Regex(@"</p><p class=""sml"">(?<Writer>.+)</p><p class=""sml"">(?<Composer>.+)</p><p id=""Lyric"">(?<Lyric>.+?)</p>", RegexOptions.Compiled);
-	}
+        private static readonly Uri QueryURI = new Uri("http://j-lyric.net/index.php");
+        private static readonly Regex SearchParseRegex = new Regex(@"<div class='title'>\s*<a href='(?<RelativePath>.+)'>(?<SongTitle>.+)</a></div>\s*<div class='status'>\s*.+<a href='.+'>(?<SongArtist>.+)</a>", RegexOptions.Compiled);
+        private static readonly Regex LyricParseRegex = new Regex(@"</p><p class=""sml"">(?<Writer>.+)</p><p class=""sml"">(?<Composer>.+)</p><p id=""Lyric"">(?<Lyric>.+?)</p>", RegexOptions.Compiled);
+    }
 }
